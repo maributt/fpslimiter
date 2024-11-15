53f4461bca617ec823d08e59d0c38b6874f52e4a
maributt <maributtocks@gmail.com>
2024-11-15 04:53:33 +0100

    api 11

--- Svc.cs
+++ Svc.cs
@@ -1,14 +1,6 @@
-﻿using Dalamud.Game;
-using Dalamud.Game.ClientState;
-using Dalamud.Game.ClientState.Conditions;
-using Dalamud.Game.Command;
-using Dalamud.Game.Gui;
-using Dalamud.Game.Text.SeStringHandling;
-using Dalamud.IoC;
+﻿using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
-using FFXIVClientStructs.FFXIV.Client.System.Framework;
-using Lumina.Excel.GeneratedSheets;
 
 namespace FPSLimiter
 {
